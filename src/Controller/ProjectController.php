ae34692ff666b2df6d9373012e848734429c867a
niyobuhungiro ellen <ellenniyobuhungiro01@gmail.com>
2022-05-17 15:51:58 +0200

    creation des page

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,24 +3,21 @@
 namespace App\Controller;
 use App\Entity\Projet;
 use App\Repository\ProjetRepository;
+use App\Form\ProjetType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProjectController extends AbstractController
 {
     #[Route('/ ', name: 'app_home')]
     public function index(ManagerRegistry $doctrine, ProjetRepository $repo): Response
     {
-        $project1 = new Projet;
-        $project1->setTitle("student portal project ");
-        $project1->setDescription("Description du projet ");    
-        $em = $doctrine->getManager();
-        $em->persist($project1);
-        $em->flush(); 
-        
-        // dump($project1);
+       
 
     return $this->render('project/index.html.twig', ['projects' => $repo->findAll()]);
     }
@@ -28,5 +25,18 @@ class ProjectController extends AbstractController
     public function show(Projet $projet): Response
     {
         return $this->render('project/show.html.twig', compact('projet'));
-    }    
+    }  
+    #[Route("/project/create", name:"app_projet_create", methods: ["GET","POST"])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $projet = new Projet;
+        $form = $this->createForm(ProjetType::class, $projet);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($projet);
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('project/create.html.twig', ['monForm' => $form->createView()]);   
+       }  
 }
